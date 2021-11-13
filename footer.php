17b8522bfa48cf45f2df289093968c7e819e5f97
priyan <45042030+priyan1995@users.noreply.github.com>
2021-11-13 10:01:23 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+<!-- Scripts -->
+<script src="js/jquery-3.5.1.min.js"></script>
+<script src="js/bootstrap.bundle.js"></script>
+
+<!-- Slick JS -->
+<script src="plugins/slick/slick.js"></script>
+
+<!-- Custom JS -->
+<script src="js/custom/custom-script.js"></script>
+
+</body>
+</html>
